e612c7ccfb4bfb7c0c689ea9e505d9b43a55037d
Sam Guyer <sam.guyer@gmail.com>
2015-12-01 16:33:54 -0500

    Working  version of "when" construct
    I figured out that “when” is simpler than the other constructs because
    it will run every single time. There is no need for a case label.

--- adel.h
+++ adel.h
@@ -11,7 +11,7 @@ struct async
   bool waiting() const { return kind == WAIT; }
   bool done() const { return kind == DONE; }
   bool hasvalue() const { return kind == VALUE; }
-  
+
   async() : kind(NONE), value(0) {}
   async(aresult k) : kind(k), value(0) {}
   async(int v) : kind(VALUE), value(v) {}
@@ -24,78 +24,64 @@ struct async
 
 #define marker(V) 					 \
   state=__LINE__;					 \
-  static int ATOKEN(T,V) = 0;			 \
-  ATOKEN(T,V) = millis();			 \
+  static int ATOKEN(T,V) = 0;				 \
+  ATOKEN(T,V) = millis();				 \
  case __LINE__:	;
-
 
 #define since(V) (millis() - ATOKEN(T,V))
 
 #define abegin static int state=0; switch (state) { case 0:
 
 #define adelay(x) {					 \
-  state=__LINE__;					 \
-  static int AVAR;				 \
-  AVAR = millis();				 \
+   state=__LINE__;					 \
+   static int AVAR;					 \
+   AVAR = millis();					 \
  case __LINE__:						 \
-   if ((millis() - AVAR) < x) return async(WAIT); \
+   if ((millis() - AVAR) < x) return async(WAIT);	 \
 }
 
 #define areturn(x) {					\
-  state=__LINE__;					\
-  return async(x);					\
- case __LINE__:	;				\
+   state=__LINE__;					\
+   return async(x);					\
+ case __LINE__:	;				        \
 }
 
 /* Every call to an async function must be made using either seq (for
    sequential semantics) or together (for parallel semantics. */
 
 #define seq(f) {					\
-  state=__LINE__;					\
- case __LINE__:					\
-  static async AVAR;				\
-  AVAR = f();					\
-  if (AVAR.waiting()) return async(WAIT);	\
+   state=__LINE__;					\
+ case __LINE__:					        \
+   static async AVAR;					\
+   AVAR = f();						\
+   if (AVAR.waiting()) return async(WAIT);		\
 }
 
 #define seqf(f, res) {					\
-  state=__LINE__;					\
- case __LINE__:					\
-  static async AVAR;				\
-  AVAR = f();					\
-  if (AVAR.waiting()) return async(WAIT);	\
-  if (AVAR.hasvalue()) res = AVAR.value;        \
+   state=__LINE__;					\
+ case __LINE__:					        \
+   static async AVAR;					\
+   AVAR = f();						\
+   if (AVAR.waiting()) return async(WAIT);		\
+   if (AVAR.hasvalue()) res = AVAR.value;		\
 }
+
+#define when(f, x)					\
+   static async AVAR;					\
+   AVAR = f();						\
+   if (AVAR.hasvalue() && AVAR.value == x)
 
 /* Our only real form of parallelism: fork-join */
-#define together(f1, f2) {				\
-  state=__LINE__;					\
- case __LINE__:					\
-   static async AVARN(1);						\
-   static async AVARN(2);						\
-   if ( ! AVARN(1).done()) AVARN(1) = f1();				\
-   if ( ! AVARN(2).done()) AVARN(2) = f2();				\
-   if (AVARN(2).waiting() || AVARN(2).waiting()) return async(WAIT);	\
+
+#define together(f1, f2) {						\
+    state=__LINE__;							\
+  case __LINE__:							\
+    static async AVARN(1);						\
+    static async AVARN(2);						\
+    if ( ! AVARN(1).done()) AVARN(1) = f1();				\
+    if ( ! AVARN(2).done()) AVARN(2) = f2();				\
+    if (AVARN(2).waiting() || AVARN(2).waiting()) return async(WAIT);	\
 }
-
-/* The semantics of when are hard to implement with macros because we need
-   to call the function f every time through the concrete execution, rather
-   than stepping through as we would with an if */
-
-#define when(f, x)							\
-  state=__LINE__;							\
- case __LINE__:								\
-   static async AVAR;							\
-   AVAR = f();								\
-   if (AVAR.waiting()) return async(WAIT);				\
-   if (AVAR.hasvalue() && AVAR.value == x)					
-
-/* We don't want a "go" operation -- then we're actually
-   dealing with threads, and that's bad 
-#define go(f)					\
-  do { \
-  state=__LINE__;
-*/
 
 #define until(c) while (! (c))
 
